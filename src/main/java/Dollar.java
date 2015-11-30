be739c1156ae13a099e92e60947df945e493f4ce
강현구 <hkkang@woowahan.com>
2015-11-30 09:53:24 +0900

    10장 흥미로운 시간

--- src/main/java/Dollar.java
+++ src/main/java/Dollar.java
@@ -2,12 +2,4 @@ public class Dollar extends Money {
     Dollar(int amount, String currency) {
         super(amount, currency);
     }
-
-    Money times(int multiplier) {
-        return Money.dollar(amount * multiplier);
-    }
-
-    String currency() {
-        return currency;
-    }
 }
--- src/main/java/Franc.java
+++ src/main/java/Franc.java
@@ -2,12 +2,4 @@ public class Franc extends Money {
     Franc(int amount, String currency) {
         super(amount, currency);
     }
-
-    Money times(int multiplier) {
-        return Money.franc(amount * multiplier);
-    }
-
-    String currency() {
-        return currency;
-    }
 }
